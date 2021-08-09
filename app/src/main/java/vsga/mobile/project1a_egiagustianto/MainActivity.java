c401d62318edff5f5cc6bb916c654d07ee22f340
egi-agustian <egiagustianto1994@gmail.com>
2021-08-10 00:43:41 +0700

    menambahkan method

--- app/src/main/java/vsga/mobile/project1a_egiagustianto/MainActivity.java
+++ app/src/main/java/vsga/mobile/project1a_egiagustianto/MainActivity.java
@@ -3,12 +3,33 @@ package vsga.mobile.project1a_egiagustianto;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //make a variable
+    TextView Hasil;
+    EditText TextNama;
+    Button btnTampil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        TextNama = (EditText) findViewById(R.id.inputNama);
+        Hasil = (TextView) findViewById(R.id.txtNama);
+        btnTampil = (Button) findViewById(R.id.btnTampilkan);
+
+        btnTampil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Hasil.setText("Nama Anda : " + TextNama.getText());
+            }
+        });
     }
 }
